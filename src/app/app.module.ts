19279b3c4e8b407fda0669c38117aef0b1483b67
Gerard Viñuela <gerardviru@gmail.com>
2022-06-09 18:11:00 +0200

    BarraLateralAdmin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraLateralAdminComponent } from './barra-lateral-admin/barra-lateral-admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraLateralAdminComponent
   ],
   imports: [
     BrowserModule,
