30ba574d829373bf2665ec10fcf05a3eb82d14f9
GerardSanchezV <gerardsanchezvelasco@gmail.com>
2022-06-09 20:15:17 +0200

    Area Admin Reserva Hecha

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AreaAdminReservaComponent } from './area-admin-reserva/area-admin-reserva.component';
 
-const routes: Routes = [];
+const routes: Routes =[]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AreaAdminReservaComponent } from './area-admin-reserva/area-admin-reserva.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AreaAdminReservaComponent
   ],
   imports: [
     BrowserModule,
