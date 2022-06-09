2d0bf5709e84c83faeb06a9b1e341455389f730f
AidaQ27 <aidaqueraltt@gmail.com>
2022-06-09 20:42:47 +0200

    mishoteles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MishotelesComponent } from './mishoteles/mishoteles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MishotelesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mishoteles/mishoteles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mishoteles',
+  templateUrl: './mishoteles.component.html',
+  styleUrls: ['./mishoteles.component.css']
+})
+export class MishotelesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
