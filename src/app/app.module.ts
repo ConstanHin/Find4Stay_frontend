19ca3937d4c6d5097bcad533b31572b1f6da7fec
Gerard Viñuela <gerardviru@gmail.com>
2022-06-09 19:40:52 +0200

    areaAdminClientesLista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AreaAdminClientesListaComponent } from './area-admin-clientes-lista/area-admin-clientes-lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AreaAdminClientesListaComponent
   ],
   imports: [
     BrowserModule,
