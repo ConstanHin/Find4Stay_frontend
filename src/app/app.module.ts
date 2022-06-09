ad59a88ff7f535834d3f34c3903f9c0835b20413
GerardSanchezV <gerardsanchezvelasco@gmail.com>
2022-06-09 19:54:19 +0200

    Seguridad Hecha

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SeguridadComponent } from './seguridad/seguridad.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SeguridadComponent
   ],
   imports: [
     BrowserModule,
