aced3f180c7910044036c12efba676fa67439a94
Gerard Viñuela <gerardviru@gmail.com>
2022-06-09 19:51:15 +0200

    AreaAdminReservasLista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AreaAdminReservasListaComponent } from './area-admin-reservas-lista/area-admin-reservas-lista.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AreaAdminReservasListaComponent,
   ],
   imports: [
     BrowserModule,
