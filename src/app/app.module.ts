02d0e4a332e675641c5af76754ff745b3e1726b7
Gerard Viñuela <gerardviru@gmail.com>
2022-06-09 17:46:32 +0200

    Área Cliente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AreaclienteComponent } from './areacliente/areacliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AreaclienteComponent
   ],
   imports: [
     BrowserModule,
