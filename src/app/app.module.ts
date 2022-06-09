a50bd2c66942f51c98fedb0d798597c06f0e4cf3
Gerard Viñuela <gerardviru@gmail.com>
2022-06-09 20:04:08 +0200

    AreaAdminEmpresasListas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AreaAdminEmpresasListasComponent } from './area-admin-empresas-listas/area-admin-empresas-listas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AreaAdminEmpresasListasComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/area-admin-empresas-listas/area-admin-empresas-listas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-area-admin-empresas-listas',
+  templateUrl: './area-admin-empresas-listas.component.html',
+  styleUrls: ['./area-admin-empresas-listas.component.css']
+})
+export class AreaAdminEmpresasListasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
