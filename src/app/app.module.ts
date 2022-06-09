125ce22b5f55b100dc55f960fd8222ea21efeec8
Gerard Viñuela <gerardviru@gmail.com>
2022-06-09 17:59:11 +0200

    BarraLateralEmpresa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraLateralEmpresaComponent } from './barra-lateral-empresa/barra-lateral-empresa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraLateralEmpresaComponent
   ],
   imports: [
     BrowserModule,
