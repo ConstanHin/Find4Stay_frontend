d0120649b75b84f43457dfc71767d3bb5c6609a8
AidaQ27 <aidaqueraltt@gmail.com>
2022-06-09 18:05:44 +0200

    areaempresa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AreaempresaComponent } from './areaempresa/areaempresa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AreaempresaComponent
   ],
   imports: [
     BrowserModule,
