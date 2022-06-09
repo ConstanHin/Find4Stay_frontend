456977f1a13befe5063e40cff59aa78615b6682e
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-06-09 18:23:52 +0200

    finalizada vista resultado busqueda hoteles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BuscadorComponent } from './buscador/buscador.component';
 import { ResultadoBusquedaHotelesModule } from './resultado-busqueda-hoteles/resultado-busqueda-hoteles.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BuscadorComponent
 
   ],
   imports: [
