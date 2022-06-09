46faacaf9df772ed90ae7361d748dfda2a6106ff
AidaQ27 <aidaqueraltt@gmail.com>
2022-06-09 20:23:47 +0200

    reservas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MisreservasComponent } from './misreservas/misreservas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MisreservasComponent
   ],
   imports: [
     BrowserModule,
