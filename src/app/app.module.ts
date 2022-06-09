ac92496b3a0a7597ab1bf1c8966c14c2505ea948
GerardSanchezV <gerardsanchezvelasco@gmail.com>
2022-06-09 20:35:43 +0200

    Admin Area Empresa Hecho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AreaAdminEmpresaComponent } from './area-admin-empresa/area-admin-empresa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AreaAdminEmpresaComponent
   ],
   imports: [
     BrowserModule,
