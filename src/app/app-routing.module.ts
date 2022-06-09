fe2cfba69cd1e1a1fa148d8fc8fd387834bfaa40
GerardSanchezV <gerardsanchezvelasco@gmail.com>
2022-06-09 18:31:34 +0200

    Panel Admin Hecho

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { areaAdminComponent } from './area-admin/area-admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'areaAdmin',
+    component:areaAdminComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/area-admin/area-admin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-area-admin',
+  templateUrl: './area-admin.component.html',
+  styleUrls: ['./area-admin.component.css']
+})
+export class areaAdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
