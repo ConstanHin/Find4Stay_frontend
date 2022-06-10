7db278ee6386b1e1e0de2861a9d829ff03aaea69
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-06-10 12:47:55 +0200

    areaadmin

---
+++ src/app/area-admin-view/area-admin-view/area-admin-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-area-admin-view',
+  templateUrl: './area-admin-view.component.html',
+  styleUrls: ['./area-admin-view.component.css']
+})
+export class AreaAdminViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
