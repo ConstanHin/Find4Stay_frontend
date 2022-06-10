90e42b023f63a53933e5c698592e925844ad269b
Gerard Viñuela <gerardviru@gmail.com>
2022-06-10 13:27:20 +0200

    View-Area-Empresa

---
+++ src/app/area-empresa-view/area-empresa-view/area-empresa-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-area-empresa-view',
+  templateUrl: './area-empresa-view.component.html',
+  styleUrls: ['./area-empresa-view.component.css']
+})
+export class AreaEmpresaViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
