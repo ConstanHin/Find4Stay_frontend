b979b172e1a0ad13eada6c835c982c6a3f058c48
AidaQ27 <aidaqueraltt@gmail.com>
2022-06-10 11:39:04 +0200

    carrusel fotos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SlidePhotosComponent } from './slide-photos/slide-photos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlidePhotosComponent
   ],
   imports: [
     BrowserModule,
