a733f820238178aceba22b47856017d2a97f7409
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-06-12 20:05:40 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AreaAdminReservaComponent } from './area-admin-reserva/area-admin-reserva.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { ResultadoBusquedaHotelesViewComponent } from './resultado-busqueda-hoteles/resultado-busqueda-hoteles-view/resultado-busqueda-hoteles-view.component';
 
 import { RegistroComponent } from './registro/registro.component';
+import { AreaAdminViewComponent } from './area-admin-view/area-admin-view/area-admin-view.component';
 
 const routes: Routes = [
 
@@ -22,7 +22,11 @@ const routes: Routes = [
   {
     path:'resultado-busqueda',
     component: ResultadoBusquedaHotelesViewComponent
-  }
+  },
+  {
+    path:'area-admin',
+    component: AreaAdminViewComponent
+  },
 
 ];
 
