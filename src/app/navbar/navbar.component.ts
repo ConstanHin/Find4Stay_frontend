5674003ce70e0dd5447925d7f5e536adf11da552
Gerard Viñuela <gerardviru@gmail.com>
2022-06-15 17:52:26 +0200

    control de la navbar una vez logueado

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  isLoggedIn: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
