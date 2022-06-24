18d6474cfa368a51baa8ab48329386eb0eed1c3d
Gerard Viñuela <gerardviru@gmail.com>
2022-06-24 22:25:26 +0200

    Impl MisReservas AreaCliente

--- src/app/area-cliente-view/area-cliente-view/misreservas/misreservas.component.ts
+++ src/app/area-cliente-view/area-cliente-view/misreservas/misreservas.component.ts
@@ -1,45 +1,31 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ReservaService } from 'src/app/service/reserva.service';
-import { CuentaService } from 'src/app/service/cuenta.service';
-import { ClienteService } from 'src/app/service/cliente.service';
+
 
 @Component({
   selector: 'app-misreservas',
   templateUrl: './misreservas.component.html',
-  styleUrls: ['./misreservas.component.css']
+  styleUrls: ['./misreservas.component.css'],
 })
 export class MisreservasComponent implements OnInit {
 
-  id?: any;
-  email?: any;
-  data: any = {"id":""};
-  datocliente: any = {"nombre":"", "apellido": "", "dni": ""};
-  datosreserva: any = {"codigo":""}
+  datoreserva: any = [{
+    codigo: '',
+    fechaEntrada: new Date(),
+    fechaSalida: new Date(),
+    hotel: '',
+    ubicacion:'',
+    importe: '',
+  }];
 
   constructor(
-    private cuentaservice: CuentaService,
-    private clienteservice: ClienteService,
-    private reservaservice: ReservaService) { }
+    private reservaservice: ReservaService
+  ) {}
 
   ngOnInit(): void {
-
-    this.cuentaservice.getauthcuenta().subscribe(
-      cuentaAuth => { this.id = cuentaAuth.id, this.email = cuentaAuth.email,
-        console.log(cuentaAuth.id);
-
-        this.clienteservice.getItem(this.id).subscribe(
-          datoscliente => { this.datocliente = datoscliente
-            console.log(datoscliente);
-
-            this.reservaservice.getItem(2).subscribe(
-              datosreserva => { this.datosreserva = datosreserva
-                console.log(this.datosreserva);
-              }
-            )
-          }
-        )
-      }
-    );
-
+    this.reservaservice.getReservaByClienteId().subscribe((datosreserva) => {
+      this.datoreserva = datosreserva;
+      console.log(this.datoreserva);
+    });
   }
 }
