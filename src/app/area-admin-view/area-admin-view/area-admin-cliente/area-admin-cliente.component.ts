9bb9a74297063b5678297599c93e4de06d020848
Constantin Vlad <67373492+ConstanHin@users.noreply.github.com>
2022-06-26 10:25:19 +0200

    editar cliente por admin

--- src/app/area-admin-view/area-admin-view/area-admin-cliente/area-admin-cliente.component.ts
+++ src/app/area-admin-view/area-admin-view/area-admin-cliente/area-admin-cliente.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { AreaService } from 'src/app/area/area.service';
 import { Cliente } from 'src/app/models/cliente';
+import { ClienteService } from 'src/app/service/cliente.service';
 
 @Component({
   selector: 'app-area-admin-cliente',
@@ -10,7 +11,7 @@ import { Cliente } from 'src/app/models/cliente';
   styleUrls: ['./area-admin-cliente.component.css']
 })
 export class AreaAdminClienteComponent implements OnInit {
-  //formDetallesCliente
+  //Form
   formDetallesCliente = new FormGroup({
     nombre: new FormControl("", Validators.required),
     apellido: new FormControl("", [Validators.required]),
@@ -20,12 +21,14 @@ export class AreaAdminClienteComponent implements OnInit {
 
   cliente: Cliente | undefined;
   disabled: boolean = true;
+  mensaje: string | undefined;
 
 
   // Inject AreaService and ActivateRoute
   constructor(
     private route: ActivatedRoute,
     private areaService: AreaService,
+    private clienteService: ClienteService,
   ) { }
 
   ngOnInit(): void {
@@ -43,6 +46,44 @@ export class AreaAdminClienteComponent implements OnInit {
     this.disabled = !this.disabled;
   }
 
+  cancelarCambios() {
+    // Resetear valores del form con los recibidos por request
+    this.formDetallesCliente.reset();
+    this.disabled = !this.disabled;
 
+  }
+
+  aceptarCambios() {
+    // guardar los cambios
+    // enviar un request put a la api para hacer el update
+    this.clienteService.update(
+      this.cliente?.id,
+      this.formDetallesCliente.value
+    ).subscribe(
+      {
+        next: v => {
+          console.log(v);
+
+          this.formDetallesCliente.reset;
+          //Mostrar mensaje éxito
+          this.mostrarMensaje("Se han modificado los datos.")
+
+        },
+        error: e => {
+          console.log(e);
+          //Mostrar mensaje fallo
+          this.mostrarMensaje("Error.")
+        }
+      }
+
+    );
+    // TODO: spinner feedaback mientras carga
+    this.disabled = !this.disabled;
+  }
+
+  mostrarMensaje(mensaje: string) {
+    this.mensaje = mensaje;
+    setTimeout(() => this.mensaje = undefined, 3500);
+  }
 
 }
