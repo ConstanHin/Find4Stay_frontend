9a7f22d4b330ca488753f603d47f42f974575741
GerardSanchezV <gerardsanchezvelasco@gmail.com>
2022-06-28 09:05:33 +0200

    mapas

--- src/app/resultado-busqueda-hoteles/resultado-busqueda-hoteles-view/Coordenadas/coords.ts
+++ src/app/resultado-busqueda-hoteles/resultado-busqueda-hoteles-view/Coordenadas/coords.ts
@@ -1,27 +1,62 @@
 export const coordenadas = [
 
   {
-    nombre: "NH",
-    lat: 41.16144022460538,
-    lon: 1.114028198300232,
+    nombre: "Hotel Gaudi",
+    lat: 41.152797,
+    lon: 1.108789,
 
   },
   {
-    nombre: "Hotel Manolo",
-    lat: 41.15825769495602,
-    lon: 1.1068398784493136,
+    nombre: "Hotel SB Ciutat de Tarragona",
+    lat: 41.118564,
+    lon: 1.244781,
 
 
   },
   {
-    nombre: "Hotel Manolo",
-    lat: 41.38871236821879,
-    lon: 2.1514308887891422,
+    nombre: "H10 Marina Barcelona",
+    lat: 41.39872,
+    lon: 2.18918,
 
   },
   {
-    nombre: "Hotel Mariana",
-    lat: 41.11856896346591,
-    lon: 1.242363054159994,
+    nombre: "H10 Urquinaona Plaza",
+    lat: 41.39361,
+    lon: 2.17226,
+  },
+  {
+    nombre: "H10 Catalunya Plaça Boutique Hotel ",
+    lat: 41.391057,
+    lon: 2.170070,
+  },
+  {
+    nombre: "Hotel Tarraco Park",
+    lat: 41.120837,
+    lon: 1.219087,
+  },
+  {
+    nombre: "AC Hotel by Marriott Tarragona ",
+    lat: 41.1239065,
+    lon: 1.2414387,
+  },
+  {
+    nombre: "Hotel Centre Reus",
+    lat: 41.1603987,
+    lon: 1.1105774,
+  },
+  {
+    nombre: "'Hotel Sercotel Reus Park",
+    lat: 41.1532522,
+    lon: 1.1123749,
+  },
+  {
+    nombre: "Hotel Centre Reus",
+    lat: 41.3863798,
+    lon: 2.1601916,
+  },
+  {
+    nombre: "Hotel SB diagonal Zero Barcelona",
+    lat: 41.4163937,
+    lon: 2.2203576,
   }
 ]
